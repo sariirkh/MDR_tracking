a27afffbdb600a54c1b4131570c6004f6cb44a45
sariirkh <puspitasarirakhmatullah@gmail.com>
2020-09-16 16:07:27 +0700

    change

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://trajekline.mif-project.com';
+$config['base_url'] = 'http://localhost/MDR_tracking/';
 
 /*
 |--------------------------------------------------------------------------
