09a49f6d1f995713fd78443c907bf8f59a49edd2
sariirkh <puspitasarirakhmatullah@gmail.com>
2020-09-17 05:17:57 +0700

    p

--- application/views/login_admin.php
+++ application/views/login_admin.php
@@ -11,7 +11,7 @@
 					<div class="card-body p-0">
 						<!-- Nested Row within Card Body -->
 						<div class="row">
-							<div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+							<div class="col-lg-6 d-none d-lg-block"> <image src="asset/img/mdr.png" width=475 height=500></div>
 							<div class="col-lg-6">
 								<div class="p-5">
 									<div class="text-center">
