d9a170e9dd7b0c8941f018200a0140ceef380d92
sariirkh <puspitasarirakhmatullah@gmail.com>
2020-09-16 19:19:42 +0700

    p

--- application/views/admin/_template/sidebar.php
+++ application/views/admin/_template/sidebar.php
@@ -33,6 +33,22 @@
     <span>Daftar Barang</span></a>
 </li>
 <!-- Nav Item - Charts -->
+<?php echo '<li class="nav-item">
+  <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+    <i class="fas fa-fw fa-cog"></i>
+    <span>Data Pengguna</span>
+  </a>
+  <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+    <div class="bg-white py-2 collapse-inner rounded">
+      <h6 class="collapse-header">Daftar Pengguna:</h6>
+      <a class="collapse-item" href="'.base_url('admin').'">Data Admin</a>
+      <a class="collapse-item" href="'.base_url('customer').'">Data User</a>
+   
+    </div>
+  </div>
+</li>';?>
+
+
 <?php echo'
  <li class="nav-item">
   <a class="nav-link" href="'.base_url('validasi/index').'">
