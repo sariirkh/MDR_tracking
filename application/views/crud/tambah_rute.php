9009da3171602de5fb5c55c6f86cdf9ca4d6edf7
sariirkh <puspitasarirakhmatullah@gmail.com>
2020-09-23 12:12:36 +0700

    dah

--- application/views/crud/tambah_rute.php
+++ application/views/crud/tambah_rute.php
@@ -8,23 +8,42 @@
  <form class="user" action="<?php echo base_url('rute/input');?>" method="post"> <!--aksi diarahkan ke function update di controller mahasiswa -->
  <div class="form-group">
  <label>Nama Kendaraan</label>
- <select class="form-control form-control-user " name="kendaraan" id="kendaraan" placeholder="Pilih Kendaraan">
-	<?php if($id_lokasi == null) :?>
-		<option value="0"> Pilih Kendaraan </option> 
-	<?php else : ?>
-		<option value="<?php echo $kendaraan ?>"><?php echo $merk_kendaraan.' - '.$pengguna; ?></option> 
-	<?php endif; ?>
-	<?php 
-	$sql="select * from kendaraan";
-	
-	$row=mysqli_query($kon,$sql);
-    $no=0;
-    while ($data = mysqli_fetch_array($row)) {
-    $no++;
-	?>
-	<?php foreach ($kendaraan as $row) ?>											
-	<option value="<?php echo $row->id_kendaraan;?>"> <?php echo $row->merk_kendaraan;?> <?php echo $row->nomor_kendaraan;?> <?php echo $row->pengguna;?> </option>												
-	<?php } ?>
+ <select class="form-control " name="kendaraan" id="kendaraan" placeholder="Pilih Kendaraan">
+ <?php
+$kon = mysqli_connect("localhost",'root',"","trackingmdr");
+if (!$kon){
+   die("Koneksi database gagal:".mysqli_connect_error());
+}
+?>
+
+<?php if($id_lokasi == null) :?>
+	<option value="0"> Pilih Kendaraan </option> 
+<?php else : ?>
+	<option value="<?php echo $merk_kendaraan ?>"><?php echo $nomor_kendaraan.' - '.$pengguna; ?></option> 
+<?php endif; ?>
+
+<?php
+$sql="select * from kendaraan";
+
+$hasil=mysqli_query($kon,$sql);
+$no=0;
+while ($data = mysqli_fetch_array($hasil)) {
+$no++;
+
+$ket="";
+if (isset($_GET['kendaraan'])) {
+	$kendaraan = trim($_GET['kendaraan']);
+
+	if ($kendaraan==$data['id_kendaraan'])
+	{
+		$ket="selected";
+	}
+}
+?>
+<option <?php echo $ket; ?> value="<?php echo $data['id_kendaraan'];?>"> <?php echo $data['merk_kendaraan'];?> (<?php echo $data['nomor_kendaraan'];?>) <?php echo $data['pengguna'];?>  </option>
+<?php } ?>	
+ 
+ 
 </select>
 
 <label>Nama Lokasi</label>
